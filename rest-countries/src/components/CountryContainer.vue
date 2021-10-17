af92a6902a502d63ec613d9c48e9fdf0fc26cd61
julianapsilva <julianadepaula001@gmail.com>
2021-10-17 08:46:28 -0300

    troca de grid para flexbox e formata populacao

--- rest-countries/src/components/CountryContainer.vue
+++ rest-countries/src/components/CountryContainer.vue
@@ -8,10 +8,8 @@
       <router-link :to="{ name: 'country', params: { name: country.name } }">
         <img :src="country.flag" alt="" />
         <ul>
-          <li>
-            <strong>{{ country.name }}</strong>
-          </li>
-          <li><strong>População: </strong>{{ country.population }}</li>
+          <li class="country-name">{{ country.name }}</li>
+          <li><strong>População: </strong>{{ population(country) }}</li>
           <li><strong>Região: </strong>{{ country.region }}</li>
           <li><strong>Capital: </strong>{{ country.capital }}</li>
         </ul>
@@ -24,6 +22,11 @@
 export default {
   props: {
     countries: Array
+  },
+  methods: {
+    population(country) {
+      return String(country.population).replace(/(.)(?=(\d{3})+$)/g, '$1,')
+    }
   }
 }
 </script>
@@ -32,19 +35,47 @@ export default {
 .countries-container {
   margin-right: 100px;
   margin-left: 100px;
-  display: grid;
-  align-items: center;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
   gap: 60px;
   line-height: 2;
 }
 .countries-container img {
-  width: 50%;
+  /* width: 50%; */
+  max-width: 300px;
+  height: 200px;
 }
 .countries-container ul li {
   list-style: none;
+  text-align: left;
 }
+.countries-container ul li strong {
+  font-weight: 600;
+}
+
+.country-name {
+  font-size: 18px;
+  margin-bottom: 5px;
+  font-weight: 800;
+  max-width: 250px;
+}
+
 .countries-item {
-  border: 1px solid;
+  box-shadow: 1px 4px 4px 0px rgba(0, 0, 0, 0.05);
+  background: hsl(0, 0%, 100%);
+  font-size: 14px;
+}
+.countries-item:hover {
+  transform: scale(1.02);
+  transition: all 0.3s;
+}
+
+@media (max-width: 1220px) {
+  .countries-container {
+    margin-right: 80px;
+    margin-left: 80px;
+    gap: 40px;
+  }
 }
 </style>
