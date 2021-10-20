e4704002c0c4071f5ce8fb97bdc02a26bd6bb8b1
julianapsilva <julianadepaula001@gmail.com>
2021-10-20 20:08:33 -0300

    instalando dependencia de icones

--- rest-countries/src/main.js
+++ rest-countries/src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import App from './App.vue'
 import router from './config/router'
 
+library.add(fas)
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .component('fa', FontAwesomeIcon)
+  .mount('#app')
